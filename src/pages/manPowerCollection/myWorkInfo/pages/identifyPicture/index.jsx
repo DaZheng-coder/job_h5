53c9170ab94b15d69a5be2ac0c786df0d2ed9c8b
zhengjunqin <zhengjunqin@shoplazza.com>
2024-05-31 11:09:51 +0800

    feat: 引入tailwindcss，并修改像素为px

--- src/pages/manPowerCollection/myWorkInfo/pages/identifyPicture/index.jsx
+++ src/pages/manPowerCollection/myWorkInfo/pages/identifyPicture/index.jsx
@@ -1,13 +1,14 @@
 import "./index.scss";
 import StepWrap from "../../../../../component/StepWrap";
 import Card from "../../../../../component/Card";
-import { Text } from "@tarojs/components";
+import { Text, View } from "@tarojs/components";
 
 export default function IdentifyPicture() {
   return (
     <StepWrap current={1}>
       <Card title="实名信息">
-        <Text>身份证人像面照片</Text>
+        <Text className="text-[28px] mt-[20px]">身份证人像面照片</Text>
+        <View className=""></View>
       </Card>
     </StepWrap>
   );
